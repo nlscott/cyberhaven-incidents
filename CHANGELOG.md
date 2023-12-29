352295881b1e11768f938b0c18d663b951770979
nic scott <nls.inbox@gmail.com>
2023-12-29 13:55:11 -0500

    v0.2.1

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+## 0.2.0
+
+ADD:
+
+- get incident details by incident id
+
+## 0.1.0
+- Initial Build
---
+++ lib/cyberhaven/incidents/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Cyberhaven
+  module Incidents
+    VERSION = "0.2.1"
+  end
+end
